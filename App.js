9af68473c02734c45a71ed3f9313a79952db2760
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-12 16:42:25 +0100

    [UPD] main component by adding some text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Very Nice to work with it</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
