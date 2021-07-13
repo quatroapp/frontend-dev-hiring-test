d918c4b657f01fd0ffc968f3d3947345723770d0
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-13 18:55:43 +0100

    [ADD] simple axios request to get list of resto

--- components/RestoList.js
+++ components/RestoList.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import splash_img from '../assets/splash_image.png';
+import axios from 'axios';
 
 export default function RestoList({ navigation }) {
+    const [resto, setResto] = useState();
+
+    useEffect(() => {
+        async function fetchResto() {
+            try {
+                const asyncResponse = await axios("https://http://localhost:3000/restos");
+                const { value } = asyncResponse.data;
+                setResto(value.resto);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchJoke();
+    }, [more]);
+
     return (
         <View style={styles.restos}>
+            <Text> Back </Text>
+            <Text> Skip </Text>
+
             <Text> Resto List </Text>
+            <Text>{`${resto}`}</Text>
             <Button
                 title="See resto details"
+                onClick={loadMore}
                 onPress={() => navigation.navigate('RestoDetails')}
             />
         </View>
