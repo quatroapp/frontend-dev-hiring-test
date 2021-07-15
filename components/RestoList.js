8393d11b90ee82530c37458698f91dd3179712f9
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-15 11:47:37 +0100

    [SETUP] map view to get current position of the user

--- components/RestoList.js
+++ components/RestoList.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, FlatList } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, FlatList, Dimensions } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function RestoList({ navigation }) {
     const [search, setSearch] = useState('');
     const [filteredResto, setFilteredResto] = useState([]);
     const [restos, setRestos] = useState([]);
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
 
     useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      })();
+
       fetch("https://my-json-server.typicode.com/blacky-yg/frontend-dev-hiring-test/restos")
         .then((response) => response.json())
         .then((responseJson) => {
@@ -18,6 +32,14 @@ export default function RestoList({ navigation }) {
           console.error(error);
         });
     }, []);
+
+    let text = 'Waiting..';
+
+    if (errorMsg) {
+      text = errorMsg;
+    } else if (location) {
+      text = JSON.stringify(location);
+    }
 
     const searchFilterFunction = (text) => {
       if (text) {
@@ -37,25 +59,25 @@ export default function RestoList({ navigation }) {
     };
 
     const RestoView = ({ item }) => {
-        return (
-            <Text style={styles.restoStyle} onPress={() => getResto(item)}>
-            {item.id}
-            {'.'}
-            {item.name.toUpperCase()}
-            </Text>
-        );
+      return (
+        <Text style={styles.restoStyle} onPress={() => getResto(item)}>
+        {item.id}
+        {'.'}
+        {item.name.toUpperCase()}
+        </Text>
+      );
     };
 
     const RestoSeparatorView = () => {
-        return (
-            <View
-                style={{
-                    height: 0.5,
-                    width: '100%',
-                    backgroundColor: '#C8C8C8',
-                }}
-            />
-        );
+      return (
+        <View
+          style={{
+            height: 0.5,
+            width: '100%',
+            backgroundColor: '#C8C8C8',
+          }}
+        />
+      );
     };
 
     const getResto = (resto) => {
@@ -63,32 +85,45 @@ export default function RestoList({ navigation }) {
     };
 
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <SearchBar
-            round
-            searchIcon={{ size: 24 }}
-            onChangeText={(text) => searchFilterFunction(text)}
-            onClear={(text) => searchFilterFunction('')}
-            placeholder="Search Resto here..."
-            value={search}
-          />
-          <FlatList
-            data={filteredResto}
-            keyExtractor={(item, index) => index.toString()}
-            ItemSeparatorComponent={RestoSeparatorView}
-            renderItem={RestoView}
-          />
-        </View>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <SearchBar
+          round
+          searchIcon={{ size: 24 }}
+          onChangeText={(text) => searchFilterFunction(text)}
+          onClear={(text) => searchFilterFunction('')}
+          placeholder="Search Resto here..."
+          value={search}
+        />
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: 6.3609772,
+            longitude: 2.4344471,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
+        <FlatList
+          data={filteredResto}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={RestoSeparatorView}
+          renderItem={RestoView}
+        />
+      </View>
     );
   };
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
+    flex: 1,
     backgroundColor: 'white',
+    justifyContent: 'center',
   },
   restoStyle: {
     padding: 10,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height - 400,
   },
 });
