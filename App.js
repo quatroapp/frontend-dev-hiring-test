c089983bc524157a63cfac4325bc49225df87f88
Hamidou TESSILIMI <hamidtessilimi@gmail.com>
2021-07-16 17:11:49 +0100

    fix differents routes stack

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from './components/Main'
 import Delivery from './components/Delivery'
 import RestoList from './components/RestoList'
-import RestoDetails from './components/RestoDetails'
 
 const RoutesStack = createStackNavigator();
 
@@ -16,7 +14,6 @@ export default function App() {
         <RoutesStack.Screen name="Main" component={Main} />
         <RoutesStack.Screen name="Delivery" component={Delivery} />
         <RoutesStack.Screen name="RestoList" component={RestoList} />
-        <RoutesStack.Screen name="RestoDetails" component={RestoDetails} />
       </RoutesStack.Navigator>
     </NavigationContainer>
   );
